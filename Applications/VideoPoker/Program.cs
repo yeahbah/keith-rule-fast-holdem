65b9f97e59b686f4b279b83facd64d3555f99dac
Arnold Diaz <yeahbah@outlook.com>
2020-11-22 00:58:15 -0700

    Updated Video Poker app to .Net 5

--- Applications/VideoPoker/Program.cs
+++ Applications/VideoPoker/Program.cs
@@ -1,7 +1,7 @@
-// This application is covered by the LGPL Gnu license. See http://www.gnu.org/copyleft/lesser.html 
-// for more information on this license.
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VideoPoker
@@ -9,11 +9,12 @@ namespace VideoPoker
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
